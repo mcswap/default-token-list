f23cdc6d269bd3ac8c24ad84b68fac1bd812ff65
mcswap <devteam@mcswap.io>
2021-06-17 16:10:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# @mcswap/default-token-list
+
+This GitHub repo contains the default token list used in the Swap interface.
+
+### Disclaimer
+
+Note filing an issue does not guarantee addition to this default token list.
+We do not review token addition requests in any particular order, and we do not
+guarantee that we will review your request to add the token to the default list.
---
+++ src/buildList.js
@@ -0,0 +1,35 @@
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+// const nile = require("./tokens/nile.json");
+// const shasta = require("./tokens/shasta.json");
+const { toChecksumAddress } = require("ethereum-checksum-address");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "MC Swap Tokens",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI:
+      "https://coin.top/production/upload/logo/TWVVcRqRmpyAi9dASvTXrqnS7FrwvDezMn.png",
+    keywords: ["HKMC", "default"],
+    // tokens: [...mainnet, ...nile, ...shasta]
+    tokens: [...mainnet]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      })
+      .map((t) => ({
+        ...t,
+        address: toChecksumAddress(t.address),
+      })),
+  };
+};
